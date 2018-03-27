3e41f4c98d8e0ed670c355ecff5ea636b2fb2e8b
hwaterke <hwaterke@users.noreply.github.com>
2018-03-27 08:52:52 +0200

    Starting point

---
+++ src/components/MovieComment.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../css/MovieComment.css';
+import {SERVER_URL} from '../constants/config';
+import axios from 'axios';
+import {connect} from 'react-redux';
+
+class _MovieComment extends React.Component {
+  static propTypes = {
+    data: PropTypes.object.isRequired,
+    updateComments: PropTypes.func.isRequired,
+    token: PropTypes.string
+  };
+
+  deleteComment = e => {
+    e.preventDefault();
+    axios
+      .delete(`${SERVER_URL}/comments/${this.props.data.id}`, {
+        headers: {Authorization: this.props.token}
+      })
+      .then(this.props.updateComments);
+  };
+
+  render() {
+    return (
+      <div className="movie-comment">
+        <span onClick={this.deleteComment}>Delete</span>
+        <p className="name">{this.props.data.author}</p>
+        <p className="comment">{this.props.data.content}</p>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  token: state.auth.token
+});
+
+export const MovieComment = connect(mapStateToProps)(_MovieComment);
